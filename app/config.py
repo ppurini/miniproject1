f6b26a6f57ec290e78f218d4a6a90b9bfd4087d6
ppurini <124829491+ppurini@users.noreply.github.com>
2023-02-15 09:53:48 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,3 +1,3 @@
 
 SECRET_KEY = 'xZxM5GMQ37CQw9kf6SRS33LadZTpSKt6'
-MONGODB_SETTINGS = {'db': 'mongodb', 'host': '0.0.0.0', 'port': 27017}  
+MONGODB_SETTINGS = {'db': 'mongodb', 'host': '10.128.0.6', 'port': 27017}  
